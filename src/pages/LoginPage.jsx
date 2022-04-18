955b41f6d93ac02492bcc446058cbf7356d1f7e1
Toczusiek <toczusieg@gmail.com>
2022-04-18 11:01:37 +0200

    #2

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,7 +8,6 @@ import CustomLink from "../components/CustomLink/CustomLink";
 
 const LoginPage = () => {
   const emailRef = useRef();
-  const errRef = useRef();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
