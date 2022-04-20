05a77e2050b4894291789e7acc30e50bef5a5abe
Maciej <mocador23@o2.pl>
2022-04-20 13:25:02 +0200

    #3

--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -3,50 +3,37 @@ import {
   Table,
   Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
   Td,
-  TableCaption,
   TableContainer,
 } from '@chakra-ui/react'
+import { RowItem } from './RowItem/RowItem'
 
 export const ProjectsList = (props) => {
 
   return (
-    <TableContainer>
+    <TableContainer mx='20px'>
       <Table size='sm'>
         <Thead>
           <Tr>
-            <Th>To convert</Th>
-            <Th>into</Th>
-            <Th isNumeric>multiply by</Th>
+            <Th>L.p.</Th>
+            <Th>ID</Th>
+            <Th>NAZWA</Th>
+            <Th>OPIS</Th>
+            <Th>UTWORZONY</Th>
+            <Th>DATA OBRONY</Th>
+            <Th>EDYCJA</Th>
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            <Td>inches</Td>
-            <Td>millimetres (mm)</Td>
-            <Td isNumeric>25.4</Td>
-          </Tr>
-          <Tr>
-            <Td>feet</Td>
-            <Td>centimetres (cm)</Td>
-            <Td isNumeric>30.48</Td>
-          </Tr>
-          <Tr>
-            <Td>yards</Td>
-            <Td>metres (m)</Td>
-            <Td isNumeric>0.91444</Td>
-          </Tr>
+          <RowItem/>
+          <RowItem/>
+          <RowItem/>
+          <RowItem/>
+          <RowItem/>
+          <RowItem/>
         </Tbody>
-        <Tfoot>
-          <Tr>
-            <Th>To convert</Th>
-            <Th>into</Th>
-            <Th isNumeric>multiply by</Th>
-          </Tr>
-        </Tfoot>
       </Table>
     </TableContainer>
   )
---
+++ src/components/ProjectsList/RowItem/RowItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Tr, Td} from '@chakra-ui/react';
+
+export const RowItem = () => {
+  return (
+    <Tr>
+        <Td>1</Td>
+        <Td>1</Td>
+        <Td>Segun Adebayo</Td>
+        <Td>Segun Adebayo</Td>
+        <Td>Segun Adebayo</Td>
+        <Td>Segun Adebayo</Td>
+        <Td>25.4</Td>
+    </Tr>
+  )
+}
