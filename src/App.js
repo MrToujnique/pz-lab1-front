1782944de06f65c4cb57aac64e9cf7fcafa590c4
Maciej <mocador23@o2.pl>
2022-06-17 19:36:21 +0200

    #10

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
 import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
-import RemindPassword from "./pages/RemindPassword";
-import ResetPassword from "./pages/ResetPassword";
-import Items from "./store/Items";
-import NewStudentPage from "./pages/NewStudentPage";
-import PeopleManager from "./pages/PeopleManager";
-import DeleteAccountPage from "./pages/DeleteAccountPage";
-import ChangePasswordPage from "./pages/ChangePasswordPage";
-import StudentManager from "./pages/StudentManager";
-import ProjectList from "./pages/ProjectList";
-import ProtectedRoutes from "./shared/utils/ProtectedRoutes";
+import { useEffect } from "react";
+import routes from "./shared/routes/routes";
+import { BrowserRouter,  Router,  useRoutes } from "react-router-dom";
+import { connect, useDispatch, useSelector } from "react-redux";
+import * as actions from "./store/actions/index";
 
 
 function App() {
+
+  const routing = useRoutes(routes());
   return (
-    <Routes>
-      <Route element={<ProtectedRoutes/>}>
-        <Route path="/kontener" element={<Items />} />
-        <Route path="/nowy-student" element={<NewStudentPage />} />
-        <Route path="/uzytkownicy" element={<PeopleManager />} />
-        <Route path="/usuwanie-konta" element={<DeleteAccountPage />} />
-        <Route path="/zmiana-hasla" element={<ChangePasswordPage />} />
-        <Route path="/studenci" element={<StudentManager />} />
-        <Route path="/projekty" element={<ProjectList />} />
-      </Route>
-      <Route path="/rejestracja" element={<RegisterPage />} />
-      <Route path="/logowanie" element={<LoginPage />} />
-      <Route path="/przypomnienie-hasla" element={<RemindPassword />} />
-      <Route path="/reset-hasla/:token" element={<ResetPassword />} />
-      <Route path="*" element={<Navigate replace to="/projekty" />} />
-    </Routes>
+    <>
+      {routing}
+    </>
   );
 }
 
-export default App;
+export default connect(null, null)(App);
--- src/shared/routes/routes.js
+++ src/shared/routes/routes.js
@@ -1,4 +1,4 @@
-import { Navigate,Outlet } from 'react-router-dom';
+import { Navigate} from 'react-router-dom';
 import RegisterPage from "../../pages/RegisterPage";
 import ProjectList  from "../../pages/ProjectList";
 import ResetPassword from "../../pages/ResetPassword";
@@ -11,20 +11,22 @@ import PeopleManager from "../../pages/PeopleManager";
 import StudentManager from "../../pages/StudentManager";
 import LoginPage from "../../pages/LoginPage";
 
-const routes = (isLoggedInd) =>[
-    { path:'/logowanie',element: !isLoggedInd?<LoginPage />:<Navigate to="/studenci"/>},
-    { path:'/rejestracja',element: !isLoggedInd?<RegisterPage />:<Navigate to="/projekty"/>},
-    { path:'/logowanie',element: !isLoggedInd?<LoginPage />:<Navigate to="/projekty"/>},
-    { path:'/przypomnienie-hasla',element: !isLoggedInd?<RemindPassword />:<Navigate to="/projekty"/>},
-    { path:'/reset-hasla/:token',element: isLoggedInd?<ResetPassword />:<Navigate to="/logowanie"/>},
-    { path:'/kontener',element: isLoggedInd?<Items />:<Navigate to="/logowanie"/>},
-    { path:'/nowy-student',element: isLoggedInd?<NewStudentPage />:<Navigate to="/logowanie"/>},
-    { path:'/uzytkownicy',element: isLoggedInd?<PeopleManager />:<Navigate to="/logowanie"/>},
-    { path:'/usuwanie-konta',element: isLoggedInd?<DeleteAccountPage />:<Navigate to="/logowanie"/>},
-    { path:'/zmiana-hasla',element: isLoggedInd?<ChangePasswordPage/>:<Navigate to="/logowanie"/>},
-    { path:'/studenci',element: isLoggedInd?<StudentManager />:<Navigate to="/logowanie"/>},
-    { path:'/projekty',element: isLoggedInd?<ProjectList />:<Navigate to="/logowanie"/>},
-    { path:'*',element: isLoggedInd?<Navigate to="/projekty" />:<Navigate to="/logowanie"/>}
+const token = localStorage.getItem("token");
+
+const routes = () =>[
+    { path:'/logowanie',element: !token?<LoginPage />:<Navigate to="/"/>},
+    { path:'/rejestracja',element: !token?<RegisterPage />:<Navigate to="/"/>},
+    { path:'/logowanie',element: !token?<LoginPage />:<Navigate to="/"/>},
+    { path:'/przypomnienie-hasla',element: !token?<RemindPassword />:<Navigate to="/"/>},
+    { path:'/reset-hasla/:token',element: token?<ResetPassword />:<Navigate to="/logowanie"/>},
+    { path:'/kontener',element: token?<Items />:<Navigate to="/logowanie"/>},
+    { path:'/nowy-student',element: token?<NewStudentPage />:<Navigate to="/logowanie"/>},
+    { path:'/uzytkownicy',element: token?<PeopleManager />:<Navigate to="/logowanie"/>},
+    { path:'/usuwanie-konta',element: token?<DeleteAccountPage />:<Navigate to="/logowanie"/>},
+    { path:'/zmiana-hasla',element: token?<ChangePasswordPage/>:<Navigate to="/logowanie"/>},
+    { path:'/studenci',element: token?<StudentManager />:<Navigate to="/logowanie"/>},
+    { path:'/',element: token?<ProjectList />:<Navigate to="/logowanie"/>},
+    { path:'*',element: token?<Navigate to="/" />:<Navigate to="/logowanie"/>}
 ]
 
 export default routes;
